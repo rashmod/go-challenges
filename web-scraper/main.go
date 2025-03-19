3c0090387715a252befd9ec3b8d45157be40bb84
Bhushan <alhatbhushan@gmail.com>
2025-03-19 15:32:31 +0530

    add worker pools

--- web-scraper/main.go
+++ web-scraper/main.go
@@ -10,32 +10,42 @@ import (
 )
 
 type Site struct {
-	url   string
-	title string
-	valid bool
-	err   error
+	workerId int
+	url      string
+	title    string
+	valid    bool
+	err      error
 }
 
 func main() {
 	urls := os.Args[1:]
 
-	ch := make(chan Site, len(urls))
+	urlChannel := make(chan string, len(urls))
+	logChannel := make(chan Site, len(urls))
 
 	var wg sync.WaitGroup
 
-	fetchAll(urls, ch, &wg)
+	for i := range 3 {
+		wg.Add(1)
+		go fetch(urlChannel, logChannel, &wg, i)
+	}
+
+	for _, url := range urls {
+		urlChannel <- url
+	}
+	close(urlChannel)
 
 	go func() {
 		wg.Wait()
-		close(ch)
+		close(logChannel)
 	}()
 
-	log(ch)
+	log(logChannel)
 }
 
-func log(ch chan Site) {
+func log(ch <-chan Site) {
 	for site := range ch {
-		fmt.Println(site.url)
+		fmt.Println("worker", site.workerId, site.url)
 		if site.valid {
 			fmt.Println(site.title)
 		} else {
@@ -43,36 +53,32 @@ func log(ch chan Site) {
 		}
 		fmt.Println()
 	}
+
 }
 
-func fetchAll(urls []string, ch chan Site, wg *sync.WaitGroup) {
-	wg.Add(len(urls))
-	for _, url := range urls {
-		go fetch(url, ch, wg)
-	}
-}
-
-func fetch(url string, ch chan Site, wg *sync.WaitGroup) {
+func fetch(urlChannel <-chan string, ch chan<- Site, wg *sync.WaitGroup, i int) {
 	defer wg.Done()
 
-	resp, err := http.Get(url)
-	if err != nil {
-		ch <- Site{url: url, valid: false, err: err}
-		return
-	}
-	defer resp.Body.Close()
+	for url := range urlChannel {
+		resp, err := http.Get(url)
+		if err != nil {
+			ch <- Site{url: url, valid: false, err: err, workerId: i}
+			continue
+		}
+		defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		ch <- Site{url: url, valid: false, err: err}
-		return
-	}
+		if resp.StatusCode != 200 {
+			ch <- Site{url: url, valid: false, err: err, workerId: i}
+			continue
+		}
 
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		ch <- Site{url: url, valid: false, err: err}
-		return
-	}
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			ch <- Site{url: url, valid: false, err: err, workerId: i}
+			continue
+		}
 
-	title := doc.Find("head title").Text()
-	ch <- Site{url: url, title: title, valid: true}
+		title := doc.Find("head title").Text()
+		ch <- Site{url: url, title: title, valid: true, workerId: i}
+	}
 }
